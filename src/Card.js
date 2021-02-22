cfdeb7aaae4b69c41c026b7c7576e1eccbdacd99
Kiefer <kiefer_8@hotmail.com>
2021-02-22 17:12:12 -0500

    a

--- src/Card.js
+++ src/Card.js
@@ -4,25 +4,12 @@ import './Card.css';
 class Card extends Component {
     render() {
         const {cardData} = this.props;
+        const {imageUrl} = cardData;
 
         return (
-            <div className="card">
-            {/* <div className="card-top">
-              <h3 className="card-name">{cardData}</h3>
-              
-            </div>
-            <div className="pic-box"> */}
-              <img src="../card-artifact.png" />
-            {/* </div>
-            <div className="card-bottom">
-              <div className="card-text-box">
-                <p className="card-text">lorem ipsum</p>
-              </div>
-            </div> */}
+          <div className="card">
+            <img className="pic-box" src={imageUrl} />
           </div>
-              // <div className="card">
-              //   <img src={cardUrl} />
-              // </div>
         );
     }
 }
